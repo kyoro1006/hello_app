3ca6c8d00efce5bd81da7983d1b163c86ebe8a6d
kyoro1006 <kyoro_1006@msn.com>
2020-05-31 17:40:22 +0900

    チュートリアル終了

--- lib/routes/talk_route.dart
+++ lib/routes/talk_route.dart
@@ -9,23 +9,58 @@ class Talk extends StatelessWidget{
         title: Text("トーク"),
       ),
       body: ListView(
-        padding: const EdgeInsets.all(8),
+        //padding: const EdgeInsets.all(8),
         children: <Widget>[
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
-          Tile(),
+          Tile(
+            icon: Icons.person,
+            username: "鹿太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "天神太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "福岡太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "博多太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "鹿太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "鹿太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "鹿太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "鹿太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "鹿太郎",
+            message: "しかし、鹿しかいない",
+          ),
+          Tile(
+            icon: Icons.person,
+            username: "鹿太郎",
+            message: "しかし、鹿しかいない",
+          ),
         ],
       )
     );
